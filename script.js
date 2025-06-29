6206b8cb31b00133388a4fe183a2b002b8424c6a
1304-PK <pushkark1304@gmail.com>
2025-06-29 17:30:31 +0530

    strikthrough text on check and added function to toggle button

--- script.js
+++ script.js
@@ -17,13 +17,16 @@ const taskDialog = document.getElementById('task-dialog')
 const projInput = document.getElementById('project-name-input')
 const taskInput = document.getElementById('task-name-input')
 const selectTag = document.getElementById('project-options')
+const toggleSidebar = document.getElementById('toggle-sidebar')
+const sideBar = document.getElementById('side-bar')
 
 function init() {
     const initialProj = new createProject('Default Project')
     projects.push(initialProj)
     initialProj.tasks.push('Task 1')
     initialProj.tasks.push('Task 2')
-    renderMainContainer()
+    updateSidebarProjects()
+    renderMainContainer(initialProj)
 }
 
 function updateTaskDialog() {
@@ -37,51 +40,73 @@ function updateTaskDialog() {
 }
 
 function updateSidebarProjects() {
-
-}
-
-function renderMainContainer() {
+    const projContainer = document.getElementById('sidebar-projects')
+    projContainer.innerHTML = ''
     projects.forEach((project) => {
-        mainContainer.innerHTML = ''
+        const btn = document.createElement('button')
+        btn.className = 'project'
+        const p = document.createElement('p')
+        p.textContent = project.name
+        btn.append(p)
 
-        const proj_name = document.createElement('h1')
-        proj_name.textContent = project.name
-
-        const taskSection = document.createElement('div')
-        taskSection.className = 'task-section'
-
-        const taskHeading = document.createElement('p')
-        taskHeading.className = 'task-heading'
-        taskHeading.textContent = 'Tasks'
-        taskSection.append(taskHeading)
-
-        project.tasks.forEach((task) => {
-            const tasks = document.createElement("div")
-            tasks.className = 'task'
-
-            const taskDetails = document.createElement('div')
-            taskDetails.className = 'task-details'
-            const taskName = document.createElement('div')
-            taskName.className = 'task-name'
-
-            const input = document.createElement('input')
-            input.type = 'checkbox'
-            const span = document.createElement('span')
-            span.textContent = task
-            taskName.append(input, span)
-
-            // const dateTime = document.createElement('div')
-            // dateTime.textContent = 'DATE AND TIME'
-
-            taskDetails.append(taskName)
-            tasks.append(taskDetails)
-
-            taskSection.append(tasks)
+        btn.addEventListener('click', () => {
+            renderMainContainer(project)
         })
 
-
-        mainContainer.append(proj_name, taskSection)
+        projContainer.append(btn)
     })
+}
+
+function renderMainContainer(project) {
+    mainContainer.innerHTML = ''
+
+    const proj_name = document.createElement('h1')
+    proj_name.textContent = project.name
+
+    const taskSection = document.createElement('div')
+    taskSection.className = 'task-section'
+
+    const taskHeading = document.createElement('p')
+    taskHeading.className = 'task-heading'
+    taskHeading.textContent = 'Tasks'
+    taskSection.append(taskHeading)
+
+    project.tasks.forEach((task) => {
+        const tasks = document.createElement("div")
+        tasks.className = 'task'
+
+        const taskDetails = document.createElement('div')
+        taskDetails.className = 'task-details'
+        const taskName = document.createElement('div')
+        taskName.className = 'task-name'
+
+        const input = document.createElement('input')
+        input.type = 'checkbox'
+
+        input.addEventListener('click', () => {
+            if (input.checked === true) {
+                tasks.classList.add('checked')
+            }
+            else {
+                tasks.classList.remove('checked')
+            }
+
+        })
+
+        const span = document.createElement('span')
+        span.textContent = task
+        taskName.append(input, span)
+
+        // const dateTime = document.createElement('div')
+        // dateTime.textContent = 'DATE AND TIME'
+
+        taskDetails.append(taskName)
+        tasks.append(taskDetails)
+
+        taskSection.append(tasks)
+    })
+
+    mainContainer.append(proj_name, taskSection)
 }
 
 // PROJECT DIALOG
@@ -95,7 +120,8 @@ document.getElementById('project-dialog-add-button').addEventListener('click', (
 
     const proj = new createProject(projInput.value)
     projects.push(proj)
-    renderMainContainer()
+    updateSidebarProjects()
+    renderMainContainer(proj)
 
     projDialog.close()
 })
@@ -113,11 +139,24 @@ document.getElementById('task-dialog-add-button').addEventListener('click', (e) 
     projects.forEach((project) => {
         if (project.name === selectTag.value) {
             project.tasks.push(taskInput.value)
+            renderMainContainer(project)
         }
     })
 
-    renderMainContainer()
     taskDialog.close()
+})
+
+let toggled = false
+
+toggleSidebar.addEventListener('click', () => {
+    if (toggled === false) {
+        sideBar.style.transform = 'translateX(0)'
+        toggled = true
+    }
+    else {
+        sideBar.style.transform = 'translateX(-60vw)'
+        toggled = false
+    }
 })
 
 init()
