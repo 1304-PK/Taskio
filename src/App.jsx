ef1b143f25ad6b332d44b8643b5709a5fc5261c7
1304-PK <pushkark1304@gmail.com>
2025-07-29 16:52:26 +0530

    pass setcurrentproject prop to tasks.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Sidebar from "./components/Sidebar"
 import Tasks from "./components/Tasks"
 import "./App.css"
@@ -6,11 +6,30 @@ import "./App.css"
 function App() {
 
   const [projects, updateProjects] = useState([])
+  const [currentProject, setCurrentProject] = useState({})
+
+  useEffect(() => {
+    updateProjects([{name: "Example Project", tasks: [{name: "Example Task"}]}])
+  }, [])
+
+  useEffect(() => {
+    if (projects.length === 1){
+      setCurrentProject(projects[0])
+    }
+  }, [projects])
 
   return (
     <div id="main-container">
-      <Sidebar projects={projects} updateProjects={updateProjects}/>
-      <Tasks />
+      <Sidebar
+        projects={projects}
+        updateProjects={updateProjects}
+        setCurrentProject={setCurrentProject} />
+
+      <Tasks
+        projects={projects}
+        updateProjects={updateProjects}
+        currentProject={currentProject}
+        setCurrentProject={setCurrentProject} />
     </div>
   )
 }
